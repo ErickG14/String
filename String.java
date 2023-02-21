f5ac9ab404df17d9019df09322fe38e3f589a0fd
ErickG14 <124231061+ErickG14@users.noreply.github.com>
2023-02-21 01:37:39 -0600

    Add files via upload
    Garcia Sebastian Luis Erick 
    ICO
    2208

---
+++ String.java
@@ -0,0 +1,28 @@
+import java.until.Scanner;
+public class String  {
+public static void main (String[] args)
+{
+
+	String[]nombre;
+	Scanner teclado = new Scanner(System.in);
+	String input = "";
+	input = teclado.nectLine();
+	nombre = input.split("");
+	for(int; 1=0; 1 < nombre.length) i++) {
+	System.out.println("nombre" + (i+1) + nombre[1]);  }
+
+	String[] profesion;
+	String input2 = "";
+	input2 = teclado.nextLine();
+	prof = input2.split("");
+	for (int; 1=0; 1 < profesion.length) i++;  {
+	System.out.println("Prodfesion" + (i+1) + profesion[1]); }
+
+	String[] pais;
+	String input3 = "";}
+	input3 = teclado.nextLine();
+	pais = input3.split("");
+	for(int 1=0; 1 < pais.length; i++) {
+	System.out.println("pais" + (i+1) + pais[1]);  }
+}
+
